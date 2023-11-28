f842746addafa4779bbbc4e59e6bb3647e940123
mounikab25 <145718690+mounikab25@users.noreply.github.com>
2023-11-27 22:08:00 -0600

    backend code for storing participants details

---
+++ regis.php
@@ -0,0 +1,46 @@
+<?php
+
+$con = mysqli_connect('localhost', 'root', '');
+
+if (!$con) {
+    echo 'Not connected to server';
+}
+
+if (!mysqli_select_db($con, 'uems')) {
+    echo 'database not selected';
+}
+
+// Check if the 'participants' table exists, and if not, create it
+$createTableSQL = "CREATE TABLE IF NOT EXISTS participants (
+    regisid INT AUTO_INCREMENT PRIMARY KEY,
+    fullname VARCHAR(255) ,
+    bannerid VARCHAR(255) NOT NULL,
+    emailid VARCHAR(255) NOT NULL,
+    eventid VARCHAR(255) NOT NULL
+)";
+
+
+if ($con->query($createTableSQL) === TRUE) {
+    echo "Table 'participants' created successfully or already exists.";
+} else {
+    echo "Error creating table: " . $con->error;
+}
+
+$fullname = $_POST['fullname'];
+$bannerid = $_POST['bannerid'];
+$emailid = $_POST['emailid'];
+$eventid = $_POST['eventid'];
+
+$sql = "INSERT INTO participants (fullname, bannerid, emailid, eventid) VALUES (?, ?, ?, ?)";
+$stmt = $con->prepare($sql);
+$stmt->bind_param("ssss", $fullname, $bannerid, $emailid, $eventid);
+
+if ($stmt->execute()) {
+    // Registration successful, redirect to signup.html with a success message
+    header("Location: Student_dash.php?success=Registration%20is%20successful");
+} else {
+    // Registration failed, redirect to signup.html with an error message
+    header("Location: register.html?error=Registration%20failed");
+}
+
+?>
