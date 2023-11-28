e9093816ec4a6bf6c574bef04f6a452d8cfbb7dd
Sreeeja9866 <sreeja.sangras@slu.edu>
2023-11-27 22:57:01 -0600

    updated code

--- updeve.php
+++ updeve.php
@@ -1,4 +1,5 @@
 <?php
+ob_start();
 // Database class for database connection and interaction
 class Database
 {
@@ -33,14 +34,14 @@ class EventManager
         $this->db = $database;
     }
  
-    public function updateEvent($eventid, $eventname, $noofparticipants, $eventdescription, $event_date, $event_time, $venue)
+    public function updateEvent($eventid, $eventname, $noofparticipants, $eventdescription, $event_date, $event_time, $venue, $targetPath, $newFileName)
     {
-        $query = "UPDATE event SET eventname='$eventname', noofparticipants='$noofparticipants', eventdescription='$eventdescription', event_date='$event_date', event_time='$event_time', venue='$venue' WHERE eventid='$eventid'";
+        $query = "UPDATE event SET eventname='$eventname', noofparticipants='$noofparticipants', eventdescription='$eventdescription', event_date='$event_date', event_time='$event_time', venue='$venue', imagepath='$targetPath', filename='$newFileName' WHERE eventid='$eventid'";
         $result = $this->db->executeQuery($query);
  
         if ($result) {
             echo "Record updated successfully";
-            exit(header("refresh:1;url=admin.html"));
+            exit(header("refresh:1;url=Admin.php"));
         } else {
             die("Error updating record: " . mysqli_error($this->db->connection));
         }
@@ -61,10 +62,39 @@ $eventdescription = $_POST['eventdescription'];
 $event_date = $_POST['event_date'];
 $event_time = $_POST['event_time'];
 $venue = $_POST['venue'];
- 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Check if the file was uploaded successfully
+    if (isset($_FILES['image']) && $_FILES['image']['error'] === UPLOAD_ERR_OK) {
+      // Get file information
+      $fileName = $_FILES['image']['name'];
+      $fileType = $_FILES['image']['type'];
+      $fileTempName = $_FILES['image']['tmp_name'];
+      $fileSize = $_FILES['image']['size'];
+  
+      // Check file type and size
+      if ($fileSize > 0 && in_array($fileType, ['image/jpeg', 'image/png', 'image/gif'])) {
+        // Generate a unique file name
+        $newFileName = md5(uniqid()) . '.' . pathinfo($fileName, PATHINFO_EXTENSION);
+  
+        // Move the uploaded file to the desired location
+        $targetPath = 'uploads/' . $newFileName;
+        if (move_uploaded_file($fileTempName, $targetPath)) {  
+          // Success message
+          echo 'Image uploaded successfully!';
+        } else {
+          echo 'Error moving uploaded file.';
+        }
+      } else {
+        echo 'Invalid file type or size.';
+      }
+    } else {
+      echo 'Error uploading file.';
+    }
+} 
 // Update the event
-$eventManager->updateEvent($eventid, $eventname, $noofparticipants, $eventdescription, $event_date, $event_time, $venue);
+$eventManager->updateEvent($eventid, $eventname, $noofparticipants, $eventdescription, $event_date, $event_time, $venue, $targetPath, $newFileName);
  
 // Close the database connection
 $db->closeConnection();
+ob_end_flush();
 ?>
